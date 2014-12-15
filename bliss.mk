ef7bace0aac3362ff71e14b0519c17d3af3e2551
nprevette420 <nprevette420@gmail.com>
2014-12-16 00:06:08 +0100

    Blissify

--- bliss.mk
+++ bliss.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/qcom-common/overlay-cm
 
@@ -12,7 +12,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Include 960x540 boot animation in the zip
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bootanimation/540.zip:system/media/540.zip
+    vendor/bliss/prebuilt/common/bootanimation/540.zip:system/media/540.zip
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -20,6 +20,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Release name
-PRODUCT_NAME := cm_moto_msm8960
+PRODUCT_NAME := bliss_moto_msm8960
 
 $(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
