af03c011475ff1c22166cb3d7895fe5dbf46a1c0
Nathan Prevette <nprevette420@gmail.com>
2014-12-20 16:35:31 -0500

    Fix video not working

--- device_moto_msm8960.mk
+++ device_moto_msm8960.mk
@@ -162,7 +162,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-scan=true \
     mmp.enable.3g2=true \
     media.aac_51_output_enabled=true \
-    ro.mot.vr_source_dsp=1
+    ro.mot.vr_source_dsp=1 \
+    qcom.hw.aac.encoder=false
 
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
