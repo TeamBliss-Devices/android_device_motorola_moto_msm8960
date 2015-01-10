d30f1883d694a284fb9bf8f36c7940038f93eb10
Nathan Prevette <nprevette420@gmail.com>
2015-01-10 12:00:25 -0500

    Remove duplicate bootanimation

--- bliss.mk
+++ bliss.mk
@@ -10,10 +10,6 @@ DEVICE_PACKAGE_OVERLAYS += device/motorola/qcom-common/overlay-cm
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
-# Include 960x540 boot animation in the zip
-PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/bootanimation/540.zip:system/media/540.zip
-
 # Media
 PRODUCT_COPY_FILES += \
     device/motorola/moto_msm8960/config/media_codecs.xml:system/etc/media_codecs.xml \
